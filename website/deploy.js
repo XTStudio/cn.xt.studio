ae10c50f685f4f3522c0f6517b77abe0e86b268a
PonyCui <cuis@vip.qq.com>
2018-11-19 14:53:31 +0800

    update deploy script.

--- website/deploy.js
+++ website/deploy.js
@@ -1,23 +1,53 @@
-var FtpDeploy = require('ftp-deploy');
-var ftpDeploy = new FtpDeploy();
+const fs = require('fs');
+const http = require('http');
+const crypto = require('crypto');
+const skey = process.env.deploy_skey
 
-var config = {
-    user: process.env.ftp_user,
-    password: process.env.ftp_password,
-    host: process.env.ftp_host,
-    port: 21,
-    localRoot: __dirname + '/build/xt-studio',
-    remoteRoot: '/htdocs/cn_xt_studio/',
-    include: ['*', '**/*'],
-    deleteRemote: false,
-    forcePasv: false
+class Deployer {
+
+    deploy() {
+        this.deployDir("build/xt-studio")
+    }
+
+    async deployDir(path) {
+        const files = fs.readdirSync(path)
+        for (let index = 0; index < files.length; index++) {
+            const it = files[index];
+            if (!fs.statSync(path + "/" + it).isDirectory()) {
+                await this.deployFile(path + "/" + it)
+            }
+            else {
+                await this.deployDir(path + "/" + it)
+            }
+        }
+    }
+
+    deployFile(path) {
+        return new Promise((resolver) => {
+            const data = fs.readFileSync(path)
+            const timestamp = (new Date().getTime() / 1000).toFixed(0)
+            const checksum = crypto.createHash("md5").update(skey + timestamp, 'utf8').digest('hex')
+            const post_options = {
+                host: 'xt.studio',
+                port: '80',
+                path: `/deploy.php?timestamp=${timestamp}&checksum=${checksum}&path=cn_xt_studio/${path.replace("build/xt-studio/", "")}`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Length': Buffer.byteLength(data)
+                }
+            };
+            const req = http.request(post_options)
+            req.write(data, () => {
+                req.end(() => {
+                    console.log("deployed: " + path)
+                    resolver()
+                })
+            })
+        })
+    }
+
 }
 
-// use with promises.
-ftpDeploy.on("uploaded", (e) => {
-    console.log("uploaded: " + e.filename)
-})
-
-ftpDeploy.deploy(config)
-    .then(res => console.log('finished'))
-    .catch(err => console.log(err))
+const deployer = new Deployer()
+deployer.deploy()
